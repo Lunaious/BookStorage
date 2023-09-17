c8e5efe6a81ecdc975620840a18b520cbf26edb9
Lunaious <52602883+Lunaious@users.noreply.github.com>
2023-09-17 15:54:01 -0500

    Update to Book class
    Included properties, constructors, and getter / setters

--- src/main/java/com/example/BookStorage/model/Book.java
+++ src/main/java/com/example/BookStorage/model/Book.java
@@ -1,4 +1,52 @@
 package com.example.BookStorage.model;
 
+import java.sql.Date;
+
 public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private Date published_date;
+
+    public Book() {
+    }
+
+    public Book(int id, String title, String author, Date published_date) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.published_date = published_date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublished_date() {
+        return published_date;
+    }
+
+    public void setPublished_date(Date published_date) {
+        this.published_date = published_date;
+    }
 }
