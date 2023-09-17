06c47e55e31693bf1f1fcd522c7778a580b1fdad
Lunaious <52602883+Lunaious@users.noreply.github.com>
2023-09-17 16:02:25 -0500

    Updated Book Controller methods
    added getAllBooks & saveBook methods

--- src/main/java/com/example/BookStorage/controller/BookController.java
+++ src/main/java/com/example/BookStorage/controller/BookController.java
@@ -1,4 +1,25 @@
 package com.example.BookStorage.controller;
 
+import com.example.BookStorage.model.Book;
+import com.example.BookStorage.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks(){
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping
+    public void saveBook(@RequestBody Book book){
+        bookService.saveBook(book);
+    }
 }
