e019a8d9cd22fe5c32d5328ad288b5979ca74010
Lunaious <52602883+Lunaious@users.noreply.github.com>
2023-09-17 16:24:56 -0500

    Implemented service logic
    service interface and repository

--- src/main/java/com/example/BookStorage/service/BookServInterface.java
+++ src/main/java/com/example/BookStorage/service/BookServInterface.java
@@ -1,4 +1,11 @@
 package com.example.BookStorage.service;
 
+import com.example.BookStorage.model.Book;
+
+import java.util.List;
+
 public interface BookServInterface {
+    List<Book> getAllBooks();
+
+    void saveBook(Book book);
 }
--- src/main/java/com/example/BookStorage/service/BookService.java
+++ src/main/java/com/example/BookStorage/service/BookService.java
@@ -1,4 +1,22 @@
 package com.example.BookStorage.service;
 
+import com.example.BookStorage.model.Book;
+import com.example.BookStorage.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks(){
+        return bookRepository.getAllBooks();
+    }
+
+    public void saveBook(Book book){
+        bookRepository.saveBook(book);
+    }
 }
