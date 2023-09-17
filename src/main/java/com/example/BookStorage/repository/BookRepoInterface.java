e70b95bc1a5a14b864211bb0ff2d3fbb2f4f103b
Lunaious <52602883+Lunaious@users.noreply.github.com>
2023-09-17 16:16:14 -0500

    Implementing repository logic
    Implemented book interface and repository

--- src/main/java/com/example/BookStorage/repository/BookRepoInterface.java
+++ src/main/java/com/example/BookStorage/repository/BookRepoInterface.java
@@ -1,4 +1,12 @@
 package com.example.BookStorage.repository;
 
+import com.example.BookStorage.model.Book;
+
+import java.util.List;
+
 public interface BookRepoInterface {
+
+    List<Book> getAllBooks();
+
+    void saveBook(Book book);
 }
